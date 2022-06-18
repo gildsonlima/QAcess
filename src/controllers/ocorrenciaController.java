35be888b4de54b09306e60f2cd7632bd837751cd
Atila Bezerra <atila.bzalm@gmail.com>
2022-06-18 14:16:32 -0300

    fix:database declaration construction

--- src/controllers/ocorrenciaController.java
+++ src/controllers/ocorrenciaController.java
@@ -6,13 +6,22 @@ package controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Statement;
+import repositors.OcorrenciaDB;
 
 /**
  *
  * @author GCL
  */
 public class ocorrenciaController implements ActionListener{
-
+    private Statement statement;
+    private OcorrenciaDB ocorrenciaDB;
+    
+    public ocorrenciaController(Statement statement, OcorrenciaDB ocorrenciaDB){
+        this.statement = statement;
+        this.ocorrenciaDB = ocorrenciaDB;
+    }
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
